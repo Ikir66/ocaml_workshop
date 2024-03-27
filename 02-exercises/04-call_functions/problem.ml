2929c9c5b674b7252f584732fbdf4c849ca16e62
Ikir66 <riki.karjalainen@aalto.fi>
2024-03-27 14:38:31 +0200

    init

--- 02-exercises/04-call_functions/problem.ml
+++ 02-exercises/04-call_functions/problem.ml
@@ -18,7 +18,9 @@ let () =
 
 (* Try to write [average] by reusing [add] and [half], first ordering your
    function invocations using parentheses, then using let bindings. *)
-let average x y = failwith "For you to implement"
+let average x y = 
+  let added = add x y in
+  half added
 
 let%test "Testing average..."  =
   Int.(=) 5 (average 5 5)
